9f2a6ef1f4a6e8c23fd95d0cbd4e1ede8497296b
Przemek O <przemyslaw.oglaza@outlook.com>
2019-09-09 10:59:55 +0200

    MyCoreApp - wyświetlenie adresu katalogu

---
+++ MyCoreApp/MyCoreApp/Pages/About.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace MyCoreApp.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Twoja katalog to  {0}.", directory);
+        }
+    }
+}
